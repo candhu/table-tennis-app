e082ebf3d8a207435e348c84ee5a4a8f65ea5248
David Wilkie <david.wilkie@sky.uk>
2024-08-04 21:50:17 +0100

    Added history menu option

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,30 @@ const { requiresAuth } = require('express-openid-connect');
 
 router.get('/table', requiresAuth(), (req, res) => {
     res.render('table', {
-        title: "SKY Table Tennis - League Table",
+        title: "League Table",
         isAuthenticated: req.oidc.isAuthenticated(),
     });
 })
 
 router.get('/fixtures', requiresAuth(), (req, res) => {
     res.render('fixtures', {
-        title: "SKY Table Tennis - Fixtures",
+        title: "Fixtures",
+        isAuthenticated: req.oidc.isAuthenticated(),
+    });
+})
+
+router.get('/history', requiresAuth(), (req, res) => {
+    const tournamentID = req.query.id;
+    const tournament = historyCache.find(item => item.id === tournamentID);
+
+    // Handle case where tournament is not found
+    if (!tournament) {
+        return res.status(404).send('Historical tournament data not found');
+    }
+
+    res.render('history', {
+        title: "History (" + tournament.start + " - " + tournament.end + ")",
+        tournament: tournament,
         isAuthenticated: req.oidc.isAuthenticated(),
     });
 })
